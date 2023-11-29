b458c9157778bf5d07377a3f84fb423b17f949ed
slimebuck <slimebuck@gmail.com>
2023-11-28 22:00:18 -0700

    Update player.gd
    old script was uploaded. This is the correct one.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -3,15 +3,16 @@ extends CharacterBody3D
 #var which_sprite
 
 	#onready vars to connect nodes for code use
-@onready var animated_sprite_2d : Node = $CanvasLayer/GunBase/AnimatedSprite2D
-@onready var raycast : Node = $head/RayCast3D
-@onready var gunsound : Node = $Gunsound
-@onready var camera_3d : Node = $head/Camera3D
-@onready var sound_footsteps : Node = $SoundFootsteps
-@onready var gun_flash : Node = $head/gun_flash
-@onready var gun_flash2 : Node = $head/gun_flash2
-@onready var flash_timer : Node = $Flash_Timer
-@onready var ui_panel : Node = $CanvasLayer/UI_Panel
+@onready var animated_sprite_2d = $CanvasLayer/GunBase/AnimatedSprite2D
+@onready var raycast = $head/RayCast3D
+@onready var gunsound = $Gunsound
+@onready var camera_3d = $head/Camera3D
+@onready var sound_footsteps = $SoundFootsteps
+@onready var deathscreen = $CanvasLayer/deathscreen
+@onready var gun_flash = $head/gun_flash
+@onready var gun_flash2 = $head/gun_flash2
+@onready var flash_timer = $Flash_Timer
+@onready var ui_panel = $CanvasLayer/UI_Panel
 
 
 	# Node Vars
@@ -57,7 +58,7 @@ var rotation_target: Vector3
 var input_mouse: Vector2
 
 	# jump control cars
-var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var previously_floored : bool = false
 var jump_single : bool = true
 var jump_double : bool = true
@@ -74,11 +75,11 @@ var forward_held : bool = false
 	#example project variables to aid example buttons
 var which_sprite 
 
-var pathkill_1 : bool 
-var pathkill_2 : bool
+var pathkill_1
+var pathkill_2
 
-@onready var sprite_1 : Node= get_tree().get_first_node_in_group("sprite_1").get_node("AnimatedSprite3D")
-@onready var sprite_2 : Node = get_tree().get_first_node_in_group("sprite_2").get_node("AnimatedSprite3D")
+@onready var sprite_1 = get_tree().get_first_node_in_group("sprite_1").get_node("AnimatedSprite3D")
+@onready var sprite_2 = get_tree().get_first_node_in_group("sprite_2").get_node("AnimatedSprite3D")
 
 func _ready():
 	
@@ -91,6 +92,42 @@ func _ready():
 	flash_timer.start()
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("restart"):
+		_on_button_button_up()
+	
+
+	# If player is dead do not allow any other actions
+	if dead:
+		return
+		
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+	if Input.is_action_just_pressed("forward"):
+		forward_held = true
+		
+	if Input.is_action_just_released("forward"):
+		forward_held = false
+		
+	if Input.is_action_just_released("forward") and is_on_floor():
+		normalspeed()
+		
+	if Input.is_action_just_pressed("left"):
+		normalspeed()
+		
+	if Input.is_action_just_pressed("right"):
+		normalspeed()
+		
+	if Input.is_action_just_pressed("back"):
+		normalspeed()
+
+	sprite_chase_button()
+
 
 func _physics_process(delta):
 	if is_on_floor() and velocity.y < 1:
@@ -99,10 +136,30 @@ func _physics_process(delta):
 	if dead:
 		return
 	velocity.y += -gravity * delta
-	var input : Vector2i = Input.get_vector("left", "right", "forward", "back")
-	var movement_dir : Vector3 = transform.basis * Vector3(input.x, 0, input.y)
+	var input = Input.get_vector("left", "right", "forward", "back")
+	var movement_dir = transform.basis * Vector3(input.x, 0, input.y)
 	velocity.x = movement_dir.x * SPEED_current
 	velocity.z = movement_dir.z * SPEED_current
+
+
+	if  Input.is_action_just_pressed("jump"):
+		if jump_single or jump_double or jump_triple:
+			Global_Audio.play("sounds/player/jump_a.ogg, sounds/player/jump_b.ogg, sounds/player/jump_c.ogg, sounds/player/jump1.wav")
+			SPEED_current += .1
+			jump_strength_current += .1
+			
+		
+		if jump_triple:
+			velocity.y += jump_strength_current
+			jump_triple = false
+			return
+		
+		if jump_double:
+			velocity.y += jump_strength_current
+			jump_double = false
+
+
+		if(jump_single): action_jump(delta)
 
 	move_and_slide()
 
@@ -128,7 +185,7 @@ func _physics_process(delta):
 	# Falling/respawning
 	
 	if position.y < -10:
-		get_tree().reload_current_scene()
+		_on_button_button_up()
 
 
 func action_jump(_delta):
@@ -149,14 +206,6 @@ func _input(event):
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$head/Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$head/Camera3D.rotation.x = clampf($head/Camera3D.rotation.x, -deg_to_rad(70), deg_to_rad(70))
-
-
-	if Input.is_action_just_pressed("exit"):
-		get_tree().quit()
-	if Input.is_action_just_pressed("restart"):
-		get_tree().reload_current_scene()
-	
-	sprite_chase_button()
 
 
 	# Fake Bunny Hop system function to return player to base speed
@@ -190,9 +239,9 @@ func _on_flash_timer_timeout():
 func _on_animated_sprite_2d_animation_finished():
 	can_shoot = true
 
-	##Despawn group types when restarting level
-#func _on_button_button_up():
-	#get_tree().reload_current_scene()
+	#Despawn group types when restarting level
+func _on_button_button_up():
+	get_tree().reload_current_scene()
 
 
 
